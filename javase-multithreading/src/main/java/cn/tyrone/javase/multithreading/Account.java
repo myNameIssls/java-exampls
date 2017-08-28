8afd952e1099215b85f44acc8b959e709d1310e7
ShangLishuai <lishuaishang@galaxyinternet.com>
2017-08-28 09:57:11 +0800

    synchronized

--- javase-multithreading/src/main/java/cn/tyrone/javase/multithreading/Account.java
+++ javase-multithreading/src/main/java/cn/tyrone/javase/multithreading/Account.java
@@ -1,12 +1,37 @@
 package cn.tyrone.javase.multithreading;
 
-
 public class Account {
+
+	private int balance = 10000; // 账户余额
 	
-	private int balance = 10000;
-	
-	public int calc(String operationType, int operationMoney){
-		return 0;
+	public int getBalance() {
+		return balance;
 	}
-	
+
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+
+	/**
+	 * 账户余额结算方法
+	 * @param operationType 操作类型
+	 * @param operationMoney 操作金额
+	 * @return
+	 */
+	public synchronized int calc(String operationType, int operationMoney) {
+
+		switch (operationType) {
+		case "add": // 添加操作
+			this.balance += operationMoney;
+			break;
+		case "reduce": // 减少操作
+			this.balance -= operationMoney;
+			break;
+		default:
+			break;
+		}
+
+		return this.balance;
+	}
+
 }
--- javase-multithreading/src/main/java/cn/tyrone/javase/multithreading/SynchronizedDemo.java
+++ javase-multithreading/src/main/java/cn/tyrone/javase/multithreading/SynchronizedDemo.java
@@ -2,29 +2,32 @@ package cn.tyrone.javase.multithreading;
 
 public class SynchronizedDemo {
 	
-	private static int balance = 1000;
-	
 	public static void main(String[] args) {
-		System.out.println("账户余额:" + balance);
 		
-		new Thread(new Runnable() {
-			
-			@Override
-			public void run() {
+		Account account = new Account();
+		
+		System.out.println("账户初始余额:" + account.getBalance());
+		
+		for (int i = 0; i < 5; i++) {
+			new Thread(new Runnable() {
 				
-			}
-		}, "addBalance-thread");
-		
-		
-		new Thread(new Runnable() {
+				@Override
+				public void run() {
+					int balance = account.calc("add", 100);
+					System.out.println("当前线程:" + Thread.currentThread().getName() + "\t账户余额-添加操作:" + balance);
+				}
+			}, "addBalance-thread").start();
 			
-			@Override
-			public void run() {
+			
+			new Thread(new Runnable() {
 				
-			}
-		}, "reduceBalance-thread");
-		
-		
+				@Override
+				public void run() {
+					int balance = account.calc("reduce", 100);
+					System.out.println("当前线程:" + Thread.currentThread().getName() + "\t账户余额-减少操作:" + balance);
+				}
+			}, "reduceBalance-thread").start();
+		}
 		
 	}
 	
