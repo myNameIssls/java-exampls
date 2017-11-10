6a384c83430c87cd4ebb3c4622278d75229eddc9
ShangLishuai <lishuaishang@galaxyinternet.com>
2017-11-10 16:35:12 +0800

    add Thread.interrupt's demo

--- javase-multithreading/src/main/java/cn/tyrone/javase/multithreading/helloword/ThreadDemo.java
+++ javase-multithreading/src/main/java/cn/tyrone/javase/multithreading/helloword/ThreadDemo.java
@@ -2,16 +2,15 @@ package cn.tyrone.javase.multithreading.helloword;
 
 /**
  * 这个类是通过继承Thread来实现多继承的
- *
  */
 public class ThreadDemo extends Thread {
-	
-	/**
-	 * 重写run方法
-	 */
-	@Override
-	public void run() {
-		System.out.println("ThreadDemo:" + Thread.currentThread().getName());
-	}
-	
+
+    /**
+     * 重写run方法
+     */
+    @Override
+    public void run() {
+        System.out.println("ThreadDemo:" + Thread.currentThread().getName());
+    }
+
 }
---
+++ javase-multithreading/src/main/java/cn/tyrone/javase/thread/ThreadInterruptDemo.java
@@ -0,0 +1,48 @@
+package cn.tyrone.javase.thread;
+
+public class ThreadInterruptDemo implements Runnable {
+
+    @Override
+    public void run() {
+
+//        System.out.println("第一次调用Thread.interrupted()" + Thread.interrupted());
+//        System.out.println("第二次调用Thread.interrupted()" + Thread.interrupted());
+
+        /**
+         * isInterrupted()
+         * 用来测试当前线程的运行状态
+         * true：表示当前线程为中断状态
+         * false：表示当前线程为运行状态
+         * 可以通过这样的方法来中断线程运行
+         */
+        if (Thread.currentThread().isInterrupted()){
+            System.out.println("由于线程状态是中断，所以return不再执行线程任务");
+            return;
+        }
+
+        while (true) {
+            try {
+                Thread.sleep(3L);
+                System.out.println("线程正在执行");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(new ThreadInterruptDemo(), "ThreadInterruptDemo");
+        // 开启线程
+        thread.start();
+
+        /**
+         * 中断当前线程
+         * 该方法仅设置当前线程的状态为中断，实际上并不中断线程的运行
+         * 如果要中断线程的运行，还需要当前线程自己中断
+         */
+        thread.interrupt();
+    }
+
+}
