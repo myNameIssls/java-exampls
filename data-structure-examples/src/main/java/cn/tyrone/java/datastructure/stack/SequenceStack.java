f64b69bf0244169f3a09cf0eca2207aeb58edbe6
尚立帅 <shanglishuai@iciyun.com>
2021-08-12 17:10:23 +0800

    增加顺序栈示例

---
+++ data-structure-examples/src/main/java/cn/tyrone/java/datastructure/stack/SequenceStack.java
@@ -0,0 +1,112 @@
+package cn.tyrone.java.datastructure.stack;
+
+import java.util.Arrays;
+
+public class SequenceStack<T> {
+
+    final int MAX_SIZE = 10;
+
+    private T[] stackArray;
+
+    private int top;
+
+    public SequenceStack(){
+        // top的初始值为-1，指向栈底，而这个top==-1也可作为栈空的标志，
+        // 每当插入一个新的栈顶元素时，先把指针top增加1，再把入栈的元素放到top指针指向的位置
+        this.top = -1;
+        this.stackArray = (T[]) new Object[MAX_SIZE];
+    }
+
+    public SequenceStack(int maxSize){
+
+        if (maxSize <= 0) {
+            System.out.println("数组长度要大于0,否则退出程序运行");
+            System.exit(1);
+        }
+
+        this.top = -1;
+        this.stackArray = (T[]) new Object[maxSize];
+    }
+
+    /**
+     * 在栈顶位置插入一个新元素
+     * @param obj
+     */
+    public void push(T obj){
+
+        // 栈满
+        if (this.top == this.stackArray.length - 1) {
+            T[] p = (T[]) new Object[this.top * 2 + 2];
+            for (int i = 0; i <= top; i++) {
+                p[i] = this.stackArray[i];
+            }
+            this.stackArray = p;
+        }
+
+        this.top++;
+        this.stackArray[top] = obj;
+    }
+
+    /**
+     * 删除栈顶元素
+     * @return
+     */
+    public T pop(){
+        if (this.top == -1) {
+            System.out.println("当前数据栈已空，无法删除元素");
+            return null;
+        }
+        this.top--;
+        return this.stackArray[this.top + 1];
+    }
+
+    /**
+     * 获取栈顶数据元素
+     * @return
+     */
+    public T getHead(){
+        if (this.top == -1) {
+            System.out.println("当前数据栈已空，无法删除元素");
+            return null;
+        }
+        this.top--;
+        return this.stackArray[this.top + 1];
+    }
+
+    /**
+     * 判断当前栈是否为空
+     * @return
+     */
+    public boolean isEmpty(){
+        return this.top == -1;
+    }
+
+    /**
+     * 获取栈中元素个数
+     * @return
+     */
+    public int size(){
+        return this.top + 1;
+    }
+
+    /**
+     * 依次访问栈中元素并输出
+     */
+    public void nextOrder(){
+        Arrays.stream(this.stackArray).forEach(t -> System.out.println(t));
+    }
+
+    /**
+     * 销毁一个已存在的栈
+     */
+    public void clear(){
+        this.top = -1;
+    }
+
+    public static void main(String[] args) {
+        SequenceStack<String> sequenceStack = new SequenceStack<>();
+        boolean empty = sequenceStack.isEmpty();
+        System.out.println(empty);
+    }
+
+}
--- multithreading-examples/src/main/java/cn/tyrone/javase/locks/InnerLockTest.java
+++ multithreading-examples/src/main/java/cn/tyrone/javase/locks/InnerLockTest.java
@@ -1,9 +1,5 @@
 package cn.tyrone.javase.locks;
 
-
-import jdk.internal.org.objectweb.asm.util.Printer;
-import org.openjdk.jol.info.ClassLayout;
-
 /**
  * Java内置锁共有4种状态：无锁状态、偏向锁状态、轻量级锁状态、重量级锁状态
  * 该示例用来演示内置锁不同状态
