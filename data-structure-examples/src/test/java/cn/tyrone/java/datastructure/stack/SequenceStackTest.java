70b99b7fd729f967e676108b3275ce7d740c469a
尚立帅 <shanglishuai@iciyun.com>
2021-08-13 09:44:31 +0800

    增加顺序栈单元测试

--- data-structure-examples/src/test/java/cn/tyrone/java/datastructure/stack/SequenceStackTest.java
+++ data-structure-examples/src/test/java/cn/tyrone/java/datastructure/stack/SequenceStackTest.java
@@ -23,57 +23,68 @@ class SequenceStackTest {
             this.sequenceStack = new SequenceStack<>();
         }
 
+        for (int i = 0; i < 5; i++) {
+            String random = RandomStringUtils.randomNumeric(2);
+            System.out.println("元素" + i + ":" + random);
+            this.sequenceStack.push(random);
+        }
+        System.out.println("-------------------------------------");
+
     }
 
     @Test
     void push() {
-        int size = 10;
-
-        for (int i = 0; i < size; i++) {
-            String random = RandomStringUtils.randomNumeric(2);
-            System.out.println("第" + (i+1) + "个元素:" + random);
-            this.sequenceStack.push(random);
+        if (this.sequenceStack.isEmpty()) {
+            this.sequenceStack.push("12313");
         }
-
-        this.sequenceStack.nextOrder();
     }
 
     @Test
     void pop() {
-
-        this.sequenceStack.push("123");
-        this.sequenceStack.push("456");
-//        System.out.println("删除之前打印");
-//        this.sequenceStack.nextOrder();
         String pop = this.sequenceStack.pop();
-        System.out.println(pop);
-//        String pop1 = this.sequenceStack.pop();
-//        System.out.println(pop1);
+        System.out.println("栈顶元素：" + pop);
+
+        System.out.println("删除栈顶元素后输出");
+
         this.sequenceStack.nextOrder();
-
-//        System.out.println("删除之后打印");
-//        this.sequenceStack.nextOrder();
-
 
     }
 
     @Test
     void getHead() {
+
+        String head = this.sequenceStack.getHead();
+        System.out.println("栈顶元素：" + head);
+
     }
 
     @Test
     void isEmpty() {
+        boolean empty = this.sequenceStack.isEmpty();
+
+        System.out.println(empty);
     }
 
     @Test
     void size() {
+        int size = this.sequenceStack.size();
+        System.out.println("this.sequenceStack共有" + size + "个元素");
     }
 
     @Test
     void nextOrder() {
+
+        this.sequenceStack.nextOrder();
+
     }
 
     @Test
     void clear() {
+
+        this.sequenceStack.clear();
+
+        boolean empty = this.sequenceStack.isEmpty();
+        System.out.println(empty);
+
     }
 }
