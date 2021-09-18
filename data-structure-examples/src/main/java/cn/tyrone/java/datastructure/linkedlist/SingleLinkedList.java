aa6ff9984aebdd6abec3a2d5b825ed31bc4a2f7e
尚立帅 <shanglishuai@iciyun.com>
2021-09-18 22:14:12 +0800

    单向链表优化

--- data-structure-examples/src/main/java/cn/tyrone/java/datastructure/linkedlist/SingleLinkedList.java
+++ data-structure-examples/src/main/java/cn/tyrone/java/datastructure/linkedlist/SingleLinkedList.java
@@ -8,42 +8,34 @@ public class SingleLinkedList<T> {
     /**
      * 当前节点
      */
-    private Node<T> currentNode;
+    private Node<T> node;
 
     /**
-     * 链表的长度
-     */
-    private int size;
-
-    public SingleLinkedList(){}
-
-    /**
-     * 在链表最后的节目上追加一个节点
+     * 向链表尾部添加
      * @param data
      * @return
      */
-    public SingleLinkedList append(T data){
+    public SingleLinkedList add(T data){
 
         Node newNode = new Node(data);
 
-        Node<T> currentNode = this.currentNode;
+        Node node = this.node;
 
         // 如果当前节点为空，则当前节点等于添加的节点
-        if (currentNode == null) {
-            this.currentNode = newNode;
-            this.size = 1;
+        if (node == null) {
+            this.node = newNode;
+            return this;
         }
 
         // 循环判断当前节点下一个节点是否为空，如果为空，为当前节点的下一下节点添加新的节点
-        while (currentNode != null) {
-            Node nextNode = currentNode.next;
+        while (node != null) {
+            Node nextNode = node.next;
             if (nextNode != null) {
-                currentNode = nextNode;
+                node = nextNode;
                 continue;
             }
             if (nextNode == null) {
-                currentNode.next = newNode;
-                this.size++;
+                node.next = newNode;
                 break;
             }
         }
@@ -52,21 +44,116 @@ public class SingleLinkedList<T> {
     }
 
     /**
+     * 通过一个数组初始化一个链表
+     * @param dataArray
+     */
+    public void initFromArrays(T[] dataArray){
+        if (dataArray == null || dataArray.length == 0) {
+            throw new RuntimeException("数组为空");
+        }
+
+        for (T data : dataArray) {
+            this.add(data);
+        }
+    }
+
+    /**
      * 返回链表长度
      * @return
      */
     public int size(){
-        return this.size;
+
+        int size = 0;
+        Node<T> node = this.node;
+        while (node != null) {
+
+            size++;
+
+            Node<T> next = node.next;
+            if (next == null) {
+                break;
+            }
+            node = next;
+        }
+        return size;
     }
 
     /**
-     * 循环输出链表中内容
+     * 当前链表是否为空
+     * @return
      */
-    public void print(){
-        while (this.currentNode != null) {
-            System.out.println(this.currentNode.data);
-            this.currentNode = this.currentNode.next;
+    public boolean isEmpty(){
+        return this.node == null;
+    }
+
+    @Override
+    public String toString() {
+
+        Node<T> node = this.node;
+
+        StringBuffer sb = new StringBuffer("[");
+        while (node != null) {
+            sb.append(node.data).append(",");
+            node = node.next;
         }
+        sb.append("]");
+
+        if (!this.isEmpty()) {
+            int lastIndexOf = sb.lastIndexOf(",");
+            sb.deleteCharAt(lastIndexOf);
+        }
+
+        return sb.toString();
+    }
+
+    /**
+     * 链表中奇数位与偶数交易位置
+     * 输入[a,1,b,2,c,3]
+     * 输出[1,a,2,b,3,c]
+     */
+    public void oddEvenSwap(){
+
+        SingleLinkedList<T> temp = new SingleLinkedList<>();
+
+        int index = 0;
+        // 奇数位节点
+        Node<T> oddNode = null;
+        // 偶数位节点
+        Node<T> evenNode = null;
+
+        Node<T> node = this.node;
+
+        while (node != null) {
+
+            index++;
+
+            // 奇数
+            if (index % 2 != 0) {
+                // 设置奇数位node
+                oddNode = node;
+
+                Node<T> next = node.next;
+                if (next != null) {
+                    index++;
+                    evenNode = next;
+                }
+
+                node = next.next;
+
+            }
+            temp.add(evenNode.data).add(oddNode.data);
+        }
+
+        this.node = temp.node;
+
+    }
+
+    /**
+     * 返回数据
+     * @return
+     */
+    public T getData(){
+        return this.node.data;
     }
 
     private class Node<T> {
--- data-structure-examples/src/test/java/cn/tyrone/java/datastructure/linkedlist/SingleLinkedListTest.java
+++ data-structure-examples/src/test/java/cn/tyrone/java/datastructure/linkedlist/SingleLinkedListTest.java
@@ -5,33 +5,67 @@ import org.junit.jupiter.api.Test;
 class SingleLinkedListTest {
 
     @Test
-    void append() {
+    void add() {
 
         SingleLinkedList<Integer> list = new SingleLinkedList<>();
-        list.append(1).append(2).append(3).append(4).append(5);
+        list.add(1).add(2).add(3).add(4).add(5);
 //        list.append(2);
 //        list.append(3);
 //        list.append(4);
 //        list.append(5);
 
-        int size = list.size();
-        System.out.println("当前链表存在" + size + "个节点");
+//        int size = list.size();
+//        System.out.println("当前链表存在" + size + "个节点");
 
-        list.print();
+//        list.print();
 
     }
 
+    /**
+     * 从数组初始化单链表
+     */
     @Test
-    void add() {
-
-        SingleLinkedList<Integer> list = new SingleLinkedList<>();
-        list.append(1);
-        list.append(2);
-        list.append(3);
-        list.append(4);
-        list.append(5);
-
-        list.print();
-
+    void initFromArrays(){
+        Object[] array = new Object[]{"a", 1, "b", 2, "c", 3};
+        SingleLinkedList<Object> singleLinkedList = new SingleLinkedList<>();
+        singleLinkedList.initFromArrays(array);
+        System.out.println(singleLinkedList.toString());
     }
+
+    /**
+     * 链表长度单元测试
+     */
+    @Test
+    void size(){
+        Object[] array = new Object[]{"a", 1, "b", 2, "c", 3};
+        SingleLinkedList<Object> singleLinkedList = new SingleLinkedList<>();
+        singleLinkedList.initFromArrays(array);
+        int size = singleLinkedList.size();
+        System.out.println("链表长度:" + size);
+    }
+
+    /**
+     * 链表中奇数位与偶数交易位置-单元测试
+     */
+    @Test
+    void oddEvenSwap(){
+        Object[] array = new Object[]{"a", 1, "b", 2, "c", 3};
+        SingleLinkedList<Object> singleLinkedList = new SingleLinkedList<>();
+        singleLinkedList.initFromArrays(array);
+        System.out.println("位置交换前:" + singleLinkedList.toString());
+        singleLinkedList.oddEvenSwap();
+        System.out.println("位置交换后:" + singleLinkedList.toString());
+    }
+
+    /**
+     * 获取数据单元测试
+     */
+    @Test
+    void getData(){
+        SingleLinkedList<Integer> list = new SingleLinkedList<>();
+        SingleLinkedList append = list.add(1).add(2);
+//        Object data = append.getData();
+//        System.out.println(data);
+    }
+
 }
